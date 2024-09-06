80857c6fd31f8408db3db267b2dc2116bf1752e6
Andrea Toska <toskaandrea@gmail.com>
2024-09-06 10:36:20 +0200

    init fr

---
+++ src/userplugins/theFunny/index.tsx
@@ -0,0 +1,13 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "The Funny",
+    description: "This does something funny.",
+    authors: [{ name: "ssdrive", id: 507605578600808449n }],
+});
